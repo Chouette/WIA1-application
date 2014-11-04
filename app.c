d7e744a5278bee276318807102a53cf8dadd4147
François REYNAUD <francois.p.reynaud@gmail.com>
2014-11-04 23:06:24 +0100

    mise en place parcours des dossiers

--- app.c
+++ app.c
@@ -3,108 +3,148 @@
 #include "files.h"
 #include "mot.h"
 #include "hashtable.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <dirent.h>
+
 #define NB_WINNERS 10
-#define DEBUG 0
+#define DEBUG 1
 
 int main(int argc, char** argv)
 {
-	int i = 0, minIndex = 0, nouveauxMots = 0, maj = 0 ;
+	int i = 0, minIndex = 0, nouveauxMots = 0, maj = 0, j = 1 ;
 	FILE* fichierEnCours = NULL ;
+	DIR* repTheme = NULL ;
+	struct dirent* entite = NULL ;
+	struct stat st ;
 	HashTable* dico = NULL ;
 	Mot winners[NB_WINNERS] ;
 	
 
 	dico = creer_dico(TAILLE) ;
 
-	/* pour chaque texte */
+	/* pour chaque dossier de thème */
 	for(i = 1 ; i < argc ; i++)
 	{
-		char* motLu = NULL ;
-
-		fichierEnCours = fopen(argv[i], "r") ;
-
-		if(fichierEnCours == NULL)
+		if(stat(argv[i], &st) == -1)
 		{
-			fprintf(stderr, "Impossible d'ouvrir %s\n", argv[i]) ;
-			exit(1) ;
+			perror("stat") ;
+			exit(errno) ;
 		}
 
-		printf("***********************\n*Lecture du fichier %d*\n***********************\n",i) ;
+		if(!S_ISDIR(st.st_mode))
+			continue ;
 
-		motLu = malloc(50 * sizeof(char)) ;
+		repTheme = opendir(argv[i]) ;
 
-		if(motLu == NULL)
+		if(repTheme == NULL)
 		{
-			fprintf(stderr, "erreur fatale : malloc() : échec de l'allocation mémoire\n") ;
-			exit(1) ;
+			perror("") ;
+			exit(errno) ;
 		}
 
-		motLu = to_lower(lire_mot(fichierEnCours, motLu)) ;	
-		/* Lire tout le texte */
-		while(motLu != NULL)
+		for(entite = readdir(repTheme) ; entite != NULL ; entite = readdir(repTheme))
 		{
-#if DEBUG
-			printf("while(motLu != NULL)\n{\n") ;
-#endif
+			char *path = NULL, *motLu = NULL ;
 
-			/* Si on a pas encore rencontré le mot */
-			if(!contient(dico, motLu))
+			motLu = malloc(50 * sizeof(char)) ;
+
+			if(motLu == NULL)
 			{
-				nouveauxMots++ ;
-#if DEBUG
-				printf("if(!contient(dico, motLu))\n{\n");
-#endif
-				Mot motAStocker = init_mot() ;
-
-#if DEBUG
-				printf("motAStocker.mot = strcpy(motAStocker.mot, motLu) ;\n");
-#endif
-				motAStocker.mot = strcpy(motAStocker.mot, motLu) ;
-
-				motAStocker.occurences = 1 ;
-				motAStocker.freq_app = 1.0/(argc-1) ;
-				update_score(&motAStocker) ;
-				motAStocker.dejaVu = i ;
-
-				insere(motAStocker, dico) ;
+				perror("malloc") ;
+				exit(errno) ;
 			}
-			else
+
+			path = malloc( (strlen(argv[i]) + strlen(entite->d_name) + 2) * sizeof(char)) ;
+
+			if(path == NULL)
 			{
-				maj++ ;
+				perror("malloc2") ;
+				exit(errno) ;
+			}
+
+			path = strcat(path, argv[i]) ;
+			path = strcat(path, "/") ;
+			path = strcat(path, entite->d_name) ;
+			fichierEnCours = fopen(path, "r") ;
+
+			if(fichierEnCours == NULL)
+			{
+				fprintf(stderr, "Impossible d'ouvrir %s\n", entite->d_name) ;
+				perror("") ;
+				exit(errno) ;
+			}
+
+			printf("***********************\n"
+				   "*Lecture du fichier %d*\n"
+				   "***********************\n",j++) ;
+
+			motLu = to_lower(lire_mot(fichierEnCours, motLu)) ;	
+
+			/* Lire tout le texte */
+			while(motLu != NULL)
+			{
 #if DEBUG
-				printf("else\n{//contient(dico,motLu)\n") ;
-#endif
-				Mot motStocke = get(motLu, dico) ;
-#if DEBUG
-				printf("get terminé\n") ;
+				printf("while(motLu != NULL)\n{\n") ;
 #endif
 
-				motStocke.occurences++ ;
-
-				if(motStocke.dejaVu != i)
+				/* Si on a pas encore rencontré le mot */
+				if(!contient(dico, motLu))
 				{
+					nouveauxMots++ ;
 #if DEBUG
-					printf("if(motStocke.dejaVu != i)\n{\n") ;
+					printf("if(!contient(dico, motLu))\n{\n");
 #endif
-					motStocke.dejaVu = i ;
-					motStocke.freq_app += 1.0 / (argc-1) ;
+					Mot motAStocker = init_mot() ;
+#if DEBUG
+					printf("motAStocker.mot = strcpy(motAStocker.mot, motLu) ;\n");
+#endif
+					motAStocker.mot = strcpy(motAStocker.mot, motLu) ;
+					motAStocker.occurences = 1 ;
+					motAStocker.freq_app = 1.0/(argc-1) ;
+					update_score(&motAStocker) ;
+					motAStocker.dejaVu = j ;
+					insere(motAStocker, dico) ;
+				}
+				else
+				{
+					maj++ ;
+#if DEBUG
+					printf("else\n{//contient(dico,motLu)\n") ;
+#endif
+					Mot motStocke = get(motLu, dico) ;
+#if DEBUG
+					printf("get terminé\n") ;
+#endif
+					motStocke.occurences++ ;
+
+					if(motStocke.dejaVu != j)
+					{
+#if DEBUG
+						printf("if(motStocke.dejaVu != i)\n{\n") ;
+#endif
+						motStocke.dejaVu = j ;
+						motStocke.freq_app += 1.0 / (argc-1) ;
+					}
+
+					update_score(&motStocke) ;
+					set(motStocke, dico) ;
+#if DEBUG
+					printf("mot stocke mis à jour\n") ;
+#endif
 				}
 
-				update_score(&motStocke) ;
+				motLu = to_lower(lire_mot(fichierEnCours, motLu)) ;	
+			}//fin while
 
-				set(motStocke, dico) ;
+			printf("\t%d nouveaux mots enregistrés\n\t%d mises a jour\n",nouveauxMots, maj) ;
+			nouveauxMots = 0 ;
+			maj = 0 ;
 #if DEBUG
-				printf("mot stocke mis à jour\n") ;
+			printf("fin while(motLu != NULL)\n") ;
 #endif
-			}
-			motLu = to_lower(lire_mot(fichierEnCours, motLu)) ;	
-		}//fin while
-		printf("\t%d nouveaux mots enregistrés\n\t%d mises a jour\n",nouveauxMots, maj) ;
-		nouveauxMots = 0 ;
-		maj = 0 ;
-#if DEBUG
-		printf("fin while(motLu != NULL)\n") ;
-#endif
+		}
 	}//fin for()
 
 	printf("\n***********************\n"
